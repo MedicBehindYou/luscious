7a20967f560e39f246da7a2643871ab39f16efeb
MedicBehindYou <byy0url3ave@gmail.com>
2024-03-10 20:57:03 -0500

    Initial commit.

---
+++ run.bat
@@ -0,0 +1,14 @@
+@echo off
+setlocal enabledelayedexpansion
+
+if not exist python\python.exe (
+	echo Python portable wasn't detected; we'll download and install it for you.
+	PowerShell -ExecutionPolicy Unrestricted -File "downloadpython.ps1"
+)
+
+cls
+echo The script can be terminated at any time by pressing Ctrl-C or clicking X
+echo -------------------------------------------------------------------------
+
+python\python.exe luscious_dl\menu.py
+pause
